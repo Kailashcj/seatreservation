662c5f9c346e7fd63928b3860a4053c7b0666511
kailashc <kailashc@vmware.com>
2020-03-11 21:43:55 +0530

    flask app

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# seatreservation
-restaurant seat booking system
+# Restaurant Seat Reservation System:
+A python, flask and mongodb based system for seat booking
+
+# Introduction:
+
+This code uses a python flask app to perform seat booking for a single restautant.
+A user of this app can perform following task using this code:
+
+1. Check Seat availability for current and future dates for below table types in a restaurant
+    - 2 seater
+    - 4 seater
+    - 6 seater
+
+2. Web reservation of required seats for a given date and time based upon availability
+3. Walk-in reservation of seats using the same web app
+4. Reservation and customer data stored persistently in mongoDB database
+
+# Requirements
+
+Following version of binaries should be installed on a linux system or python virtualenvironment
+
+- Flask==1.1.1
+- pymongo==3.5.1
+- Python3==3.6.8
+- MongoDB shell version v4.2.3
+
+# Assumptions
+
+- A single non-chained restaurant with 42 seats
+- App follows the table booking system
+- 42 seats = 10 (2 seater table * 5 ), 20 (4 seater table * 5 ), 12 (6 seater table * 2)
+- A 24 hour clock time for calculating time for reservation
+- A sample time window between 08:00AM to 10:00AM. A booking slot is available every 30 minutes between this time window.
+- Default booking time is 90 minutes per customer. This means a customer booking a slot for '0800' on table t1, would continue to use the table until '0930'. Thus this table t1 can be next booked at '0930'.
+
+# Known Limitations
+
+- Dummy customer id's is used in the code
+- Seat cancellation and releasing the seat back to available pool is not implemented
+- Claim seat feature, which releases the seat to available pool if a customer doesn't show-up within 30 minutes is not implemented
+- releasing a reservation at the time of customer billing is not implemented. This feature would help to identify making seats available for walk-ins.
+
+# Usage
+
+1. Install listed requirements on a linux system
+2. run mongo on a terminal by typing command $ mongo
+3. run flask app on a terminal by typing command $ python3 app.py
+4. Open a browser and run below url's:
+    - Check Seat availability: http://127.0.0.1:5000/checkseatavailability?date=2020-03-15
+    - Book Seat : http://127.0.0.1:5000/book?date=2020-03-14&time=1000&customer_id=y&num_people=2
---
+++ app.py
@@ -0,0 +1,145 @@
+'''
+This file has python code for
+reserving seats in a restaurant.
+
+'''
+from flask import Flask
+from flask import request
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+
+app = Flask(__name__)
+
+# mongodb connection
+client = MongoClient('mongodb://127.0.0.1:27017')
+try:
+    client.admin.command('ismaster')
+    print('MongoDB connection: Success')
+except ConnectionFailure as cf:
+    print('MongoDB connection: failed', cf)
+
+'''
+- A total of 12 tables in the restaurant. 2 seaters : 5, 4 seaters; 5, 6 seaters: 2
+- sample slots for seat reservation.
+- booking defaults to 90 minutes. 
+- next slot is available after 90 minutes on the same table
+- Follows a 24 hour clock and 10:00AM is last booking available in the sample below
+'''
+
+tables = ['t1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9', 't10', 't11', 't12']
+all_slots = ['0800', '0830', '0900', '0930', '1000']
+tslots = {
+    '0800': ['0800', '0830', '0900'],
+    '0830': ['0830', '0900', '0930'],
+    '0900': ['0900', '0930', '1000'],
+    '0930': ['0930', '1000'],
+    '1000': ['1000']
+}
+
+@app.route('/checkseatavailability')
+def check_availability():
+    date = request.args.get('date')
+    available = {2: {}, 4 :{}, 6: {}}
+    current_bookings = {}
+    db = client['seats'] # handle for 'seats' db
+    query_result = db['bookings'].find({'date': date})
+    for results in query_result:
+        if results['table'] not in current_bookings:
+            current_bookings[results['table']] = results['slots']
+    
+    for t in tables:
+        if t not in current_bookings: # if tables are empty. show all slots
+            if int(t[1:]) <= 5:
+                available[2][t] = all_slots
+            elif int(t[1:]) <=10:
+                available[4][t] = all_slots
+            elif int(t[1:]) <= 12:
+                available[6][t] = all_slots
+        else:
+            for k in current_bookings: # for blocked tables, check possible available slots
+                temp = []
+                for time in current_bookings[k]:
+                    temp.append(time)
+                sublist = list(set(all_slots) - set(temp))
+                if len(sublist) >= 3:
+                    for s in tslots:
+                         if set(tslots[s]) >= set(sublist):
+                             if int(k[1:]) <= 5:
+                                 available[2][k] = s
+                             elif int(k[1:]) <= 10:
+                                 available[4][k] = s
+                             elif int(k[1:]) <= 12:
+                                 available[6][k] = s
+    return available
+
+#tables availabile for 2, 4 and 6 seats
+def get_possible_tables(num_people):
+    if num_people <= 2:
+        return ['t1', 't2', 't3', 't4', 't5']
+    elif num_people <= 4:
+        return ['t6', 't7', 't8', 't9', 't10']
+    elif num_people <= 6:
+        return ['t11', 't12']
+    else:
+        return []
+
+def is_booking_possible(table_detail, time):
+    if 'slots' not in table_detail: # slots are empty
+        return True
+    slots = table_detail['slots']
+    booking_slots = tslots[time]
+    for booking_slot in booking_slots:
+        if booking_slot in slots:
+            return False
+    return True
+
+def do_booking(table_detail, time, customer_id):
+    if 'slots' not in table_detail:
+        table_detail['slots'] = {}
+    slots = table_detail['slots']
+    booking_slots = tslots[time]
+    for booking_slot in booking_slots:
+        slots[booking_slot] = customer_id
+    
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/book')
+def book():
+    date = request.args.get('date')
+    time = request.args.get('time')
+    customer_id = request.args.get('customer_id')
+    num_people = int(request.args.get('num_people'))
+
+    possible_tables = get_possible_tables(num_people)
+    db = client['seats'] # handle for 'seats' db
+
+    booking_done = False
+    for possible_table in possible_tables:
+        query_result = db['bookings'].find({'date': date, 'table': possible_table})
+        table_detail = {}
+        for result in query_result:
+            table_detail = result
+
+        # if table is empty
+        if not table_detail:
+            table_detail = {'date': date, 'table': possible_table}
+            do_booking(table_detail, time, customer_id)
+            db['bookings'].insert(table_detail)
+            booking_done = True
+            break
+        else:
+            possible = is_booking_possible(table_detail, time)
+            if possible:
+                do_booking(table_detail, time, customer_id)
+                db['bookings'].update({'date': date, 'table': possible_table}, table_detail)
+                booking_done = True
+                break
+    if booking_done:
+        return "Booking done"
+    else:
+        return "Booking not possible"
+
+if __name__ == '__main__':
+    app.run()
